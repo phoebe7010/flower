5d35bf246c7f0793fccd8de85d26694c8a9766c0
phoebe7010 <phoebe7010@daum.net>
2021-09-16 17:16:04 +0900

    step1

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,33 @@
- 
+//DOM Caching 
+const $wrap = $("#wrap"); 
+const $btns = $wrap.find(".left ul li"); 
+const $boxs = $wrap.find(".right>div"); 
+let speed = 500; 
+let isDone = true; 
+
+//event binding
+$btns.on("click", function(e){
+    e.preventDefault(); 
+
+    let isOn = $(this).chilren("a").hasClass("on"); 
+    if(isOn) return; 
+
+    if(isDone){
+        isDone = false; 
+        let i = $(this).index(); 
+        console.log(i); 
+    
+        $btns.children("a").removeClass("on"); 
+        $btns.eq(i).children("a").addClass("on"); 
+    
+        $boxs.fadeOut(speed); 
+        $boxs.eq(i).fadeIn(speed, function(){
+            isDone = true; 
+        });
+    }
+    
+}); 
+
 
 
 
